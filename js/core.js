924c9e55220a9f97f1b7411998920ba5c6eb4efd
Mario de Frutos <ethervoid@gmail.com>
2013-03-02 21:18:15 +0100

    infowindow fixed position, solved bug that add html from other countries

--- js/core.js
+++ js/core.js
@@ -1,4 +1,4 @@
-/*global Parse,cartodb,_ */
+/*global L,Parse,cartodb,_ */
 
 var YourCommitment = YourCommitment || {};
 
@@ -27,22 +27,36 @@ var YourCommitment = YourCommitment || {};
             });
             this.paintVisualization();
         },
+        position : null,
         paintVisualization : function () {
             var that = this;
             cartodb.createVis('map', 'http://yourcommitment.cartodb.com/api/v1/viz/4701/viz.json', this.options)
               .done(function (vis, layers) {
                     layers[1].setInteractivity(['country', 'iso3', 'poverty_headcount_ratio_at_2']);
                     layers[1].on('featureClick', function (e, latlng, pos, data) {
+                        that.position = latlng;
                         var parse_query = new YourCommitment.Query();
                         parse_query.query("ONGCountry", data.iso3, function (results) {
+                            console.log(results);
+                            $(".ong_list").html("");
                             _.each(results, function (ong) {
-                                $(".ong_list").append("<li class='ong_name'><a href='ong.html?ongId=" + ong.id + "&country=" + data.iso3  + "'> " + ong.attributes.GNO + "</a><span>2</span></li>");
+                                if (ong.attributes.ONG !== undefined) {
+                                    $(".ong_list").append("<li class='ong_name'><a href='ong.html?ongId=" + ong.attributes.ONG.id + "&country=" + data.iso3  + "&lat=" + that.position[0] + "&lng=" + that.position[1] + "'> " + ong.attributes.GNO + "</a><span>2</span></li>");
+                                } else {
+                                    $(".ong_list").append("<li class='ong_name'><a href='#'> " + ong.attributes.GNO + "</a></li>");
+                                }
                             });
                             $(".country_info_window").css("top", that.click_position.y - 280);
                             $(".country_info_window").css("left", that.click_position.x - 130);
                             $(".country_name").text(data.country);
                             $(".country_info_window").fadeIn(200);
                         });
+                    });
+                    vis.getNativeMap().on("move", function (e) { 
+                        if (that.position !== null) {
+                            var p = vis.getNativeMap().latLngToContainerPoint(new L.LatLng(that.position[0], that.position[1]));
+                            $(".country_info_window").css({"top": p.y - 280, "left": p.x - 130});
+                        }
                     });
                 });
         }
@@ -74,7 +88,4 @@ function init() {
     yourcommitment.show();
 }
 
-
-
 window.onLoad = init();
-
